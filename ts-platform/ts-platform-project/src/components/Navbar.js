782523425e617977de7bd2e3ccff3710deb76ac4
artwol993 <artwol993@gmail.com>
2020-10-16 17:47:58 +0200

    deleted button from navbar

--- ts-platform/ts-platform-project/src/components/Navbar.js
+++ ts-platform/ts-platform-project/src/components/Navbar.js
@@ -1,37 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Button } from './Button';
+
 
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
-
-    useEffect(() => {
-        showButton();
-    }, [])
-
-    window.addEventListener('resize', showButton);
 
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo onClick={closeMobileMenu'>
+                    <Link to='/' className='navbar-logo onClick={closeMobileMenu}'>
                         <i class="fas fa-truck"></i>
-                    TS-Platform
+                        TS-Platform
                     </Link>
 
                     <div className='menu-icon' onClick={handleClick}>
@@ -54,7 +41,7 @@ function Navbar() {
                         </li>
 
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'> Dodaj ładunek</Button>}
+
                 </div>
             </nav>
         </>
