0e45c5d00f17f02124b0ae891022f20d36d5a1cc
artwol993 <artwol993@gmail.com>
2020-10-19 23:42:36 +0200

    started footer

--- ts-platform/ts-platform-project/src/App.js
+++ ts-platform/ts-platform-project/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
         </Switch>
+        <Footer />
       </Router>
     </>
   );
