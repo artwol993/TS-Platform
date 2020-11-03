89c4f936904e06a95061d50eda072a2a86365872
artwol993 <artwol993@gmail.com>
2020-11-03 17:52:55 +0100

    init

---
+++ .history/src/components/pages/AddLoad_20201028210819.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './AddLoad.css';
+
+
+function AddLoad() {
+    return (
+        <div className='add-load-section'>
+            <div className='add-load'>
+
+                <button>Nowa oferta</button>
+
+                <div className='add-load-container'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddLoad
